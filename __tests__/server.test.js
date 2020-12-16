337a300c4121eab033c08a5f845046354470c2f6
Ashley Moore <mooream2291@gmail.com>
2020-12-16 11:54:49 -0500

    uML added

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -8,10 +8,9 @@ const mockRequest = supertest(server);
 
 describe('web server', () => {
 
-    // it('should respond with a 404 on an indvalid method', async () => {
-
-    //     expect(response.status).toBe(404);
-    // });
+    it('should respond with a 404 on an indvalid method', async () => {
+        expect(response.status).toBe(404);
+    });
 
     it('can add a record', async() => {
         return mockRequest.post('/place')
